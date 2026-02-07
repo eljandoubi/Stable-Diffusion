09c5c558f0b0ccc369e5540c986a2f9deace7792
ejandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-02-07 16:09:42 +0100

    dowload data

--- lpips_trainer.py
+++ lpips_trainer.py
@@ -8,5 +8,23 @@ from torchvision import transforms
 from PIL import Image
 import argparse
 from accelerate import Accelerator
-
+from dotenv import load_dotenv
 from modules.lpips import LPIPS, DiffToLogits
+
+load_dotenv()
+
+
+def download_data(path: str = "./data"):
+
+    import kaggle
+
+    kaggle.api.dataset_download_files(
+        "chaitanyakohli678/berkeley-adobe-perceptual-patch-similarity-bapps",
+        path=path,
+        unzip=True,
+        quiet=False,
+    )
+
+
+if __name__ == "__main__":
+    download_data()
