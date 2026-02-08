c485b5b5f67e29c47234a90cc5d6ede300eb19c1
ejandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-02-09 00:38:24 +0100

    adapt args

--- scripts/tsv2parquet.sh
+++ scripts/tsv2parquet.sh
@@ -4,8 +4,8 @@ img2dataset --url_list data/Train_GCC-training_with_head.tsv \
             --url_col "url" \
             --caption_col "caption" \
             --output_format parquet \
-            --output_folder data/GCC \
-            --processes_count 32 \
+            --output_folder data/GCC/raw \
+            --processes_count 16 \
             --thread_count 128 \
             --image_size 256 \
             --resize_mode keep_ratio
