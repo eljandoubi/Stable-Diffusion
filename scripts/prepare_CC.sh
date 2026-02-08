4d0a1d169f4d32ee2f029db8b3e9c1009f00b918
ejandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-02-09 00:07:34 +0100

    prepare Conceptual Captions dataset

---
+++ scripts/prepare_CC.sh
@@ -0,0 +1,8 @@
+uv run prepare_CC.py --path_to_data_root data/GCC \
+                  --path_to_save data/GCC/hf_train_unencoded \
+                  --hf_clip_model_name openai/clip-vit-large-patch14 \
+                  --hf_cache_dir data/GCC/hf_cache \
+                  --cpu_batch_size 512 \
+                  --gpu_batch_size 256 \
+                  --num_cpu_workers 16 \
+                  --dtype bfloat16
